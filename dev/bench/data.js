2e9247befc7c8f4bcd21841c67909ac5150d101e
github-action-benchmark <github@users.noreply.github.com>
2023-07-30 08:23:12 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for bce491700ba3728f331ea31e4a1785d443c9b8df

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1690705392026,
+  "repoUrl": "https://github.com/bajpai244/test-benchmark-action",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "bajpaiharsh244@gmail.com",
+            "name": "bajpai244",
+            "username": "bajpai244"
+          },
+          "committer": {
+            "email": "bajpaiharsh244@gmail.com",
+            "name": "bajpai244",
+            "username": "bajpai244"
+          },
+          "distinct": true,
+          "id": "bce491700ba3728f331ea31e4a1785d443c9b8df",
+          "message": "update data",
+          "timestamp": "2023-07-30T13:52:50+05:30",
+          "tree_id": "d83c7e998e8c7c9036f71fd33f207e8875ced701",
+          "url": "https://github.com/bajpai244/test-benchmark-action/commit/bce491700ba3728f331ea31e4a1785d443c9b8df"
+        },
+        "date": 1690705390969,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      }
+    ]
+  }
+}
